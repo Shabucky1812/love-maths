c502f7487614b048d4e67020b0b77f42858f6297
Shaun Buck <shaunbuck1812@gmail.com>
2022-10-25 17:19:25 +0000

    Add code to add to score tally and incorrect tally accordingly

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,8 +51,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Right :)");
+        incrementScore();
     } else {
         alert(`Wrong :( you answered ${userAnswer}, the correct answer was ${calculatedAnswer[0]}`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -79,11 +81,24 @@ function calculateCorrectAnswer() {
 
 }
 
+
+/**
+ * Gets the current score from the DOM and adds 1.
+ */
 function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
 
 }
 
+/**
+ * Gets the current number of incorrect answers and adds 1.
+ */
 function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldScore;
 
 }
 
